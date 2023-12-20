d3725d1ee614e3ec6a3f086f09a22a458089eb91
Reza Kamali <mrgando@protonmail.com>
2023-12-20 23:48:55 +0330

    connection method changed

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-** How To Run ? **
+# what's this
 
-1. After cloning project, run below command in terminal to install dependencies :
+Node.js Telegram Bot Template - if you live in Iran or Russia and has troubles connecting to Telegram servers this project might be what you're looking for.
 
-`npm i`
+# How To Run
 
-2. You can use `nodemon bot.js` or `node bot.js` command to run the bot.
+1. After cloning project, run this command `npm i` in terminal to install dependencies.
 
-you are all set,
-goodluck 👍
+2. visit Ngrok website at `ngrok.com` and signup, download the app, open it and use your token to log in ( Token is provided in your ngrok account ).
+
+3. run a server by typing `ngrok http [Port Number e.g 5000]` ngrok will create a tunnel for you and gives you the link, something like this :
+   `https://6a3a-69-12-94-107.ngrok-free.app`, leave the tunnel open and copy the link in .env file as `SERVER_URL`.
+
+4. copy your Telegram bot token in .env file as `TOKEN`.
+
+5. Don't forget to run VPN on your mechine ( only for Iran and Russia )
+
+6. You can use `npm run Server` command on a second terminal screen to run the bot.
+
+7. you have to see a log like this `{ ok: true, result: true, description: 'Webhook is already set' }` in your terminal.
+
+   you are all set,
+   goodluck 👍
--- bot.js
+++ bot.js
@@ -1,44 +1,29 @@
-const TelegramBot = require('node-telegram-bot-api');
-const Agent = require('socks5-https-client/lib/Agent');
-require("dotenv/config")
+require("dotenv/config");
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.TOKEN;
+const { TOKEN, SERVER_URL } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {
-    polling: true, request: {
-        agentClass: Agent,
-        agentOptions: {
-            socksHost: process.env.HOST,
-            socksPort: process.env.PORT,
-        },
-    }
-});
+const app = express();
+app.use(bodyParser.json());
 
-// Matches "/echo [whatever]"  
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram 
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+const init = async () => {
+    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+    console.log(res.data);
+}
 
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
+// Accept requests
+app.post(URI, async (req, res) => {
+    console.log(req.body);
+    return res.send();
+})
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
-
-
-bot.on("polling_error", (msg) => console.log(msg));
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-});
+app.listen(process.env.PORT || 5000, async () => {
+    console.log(`App running on port `, process.env.PORT || 5000);
+    await init();
+})
